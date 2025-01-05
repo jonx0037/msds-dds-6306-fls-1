5d8525464e04c2f24c48adbce2bfacfd0936e234
Jonathan Rocha <jonx0037@gmail.com>
2025-01-05 13:34:50 -0600

    Update GitHub Actions workflow for deployment and enhance .gitignore

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/MSDS_6306_Doing-Data-Science' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/msds-dds-6306-fls-1' : '',
   images: {
     unoptimized: true
   }
