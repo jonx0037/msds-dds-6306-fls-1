30a7ed0acdf23d5e24a2b7f5d856292d5d165bec
Jonathan Rocha <jonx0037@gmail.com>
2025-01-05 14:05:10 -0600

    Update GitHub Actions workflow for Next.js deployment and adjust configuration settings

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.GITHUB_ACTIONS ? '/msds-dds-6306-fls-1' : '',
-  assetPrefix: process.env.GITHUB_ACTIONS ? '/msds-dds-6306-fls-1/' : '',
+  basePath: '/msds-dds-6306-fls-1',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
   reactStrictMode: true,
 }
 
